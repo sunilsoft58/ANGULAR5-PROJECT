a1bf27df4df47b40790a255f69f9ee1b9924cc9d
sunil <sunilsoft58@gmail.com>
2018-04-29 15:23:23 +0530

    ad

--- Directive-Demo/src/app/attribute-directives/ng-class-direvtive/ng-class-direvtive.component.ts
+++ Directive-Demo/src/app/attribute-directives/ng-class-direvtive/ng-class-direvtive.component.ts
@@ -2,8 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ng-class-direvtive',
-  templateUrl: './ng-class-direvtive.component.html',
-  styleUrls: ['./ng-class-direvtive.component.css']
+  //templateUrl: './ng-class-direvtive.component.html',
+  styleUrls: ['./ng-class-direvtive.component.css'],
+  template:`<p ngClass="lefttext backcolor textfont"> Assigning as a string ... </p>  
+<p [ngClass]="['centertext','secondparabgcolor','secondparafont']"> assigning as array </p>  
+<p [ngClass]="{ borderandBox: false, dark: false, changefont: changefont }"> Assigning as Object. <br> 
+<button type="button" (click)="changefont=!changefont">change class On click.</button> </p>  
+  `
 })
 export class NgClassDirevtiveComponent implements OnInit {
 
--- Directive-Demo/src/app/attribute-directives/ng-style-direvtive/ng-style-direvtive.component.ts
+++ Directive-Demo/src/app/attribute-directives/ng-style-direvtive/ng-style-direvtive.component.ts
@@ -2,14 +2,36 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ng-style-direvtive',
-  templateUrl: './ng-style-direvtive.component.html',
-  styleUrls: ['./ng-style-direvtive.component.css']
+//  templateUrl: './ng-style-direvtive.component.html',,
+  styleUrls: ['./ng-style-direvtive.component.css'],
+  template:`<p style="padding: 1rem" [ngStyle]="{'color': Red,'font-weight': 'Bold','font-size':'35px','borderBottom': borderStyle}"> Assigning Inline style to ngStyle </p>  
+<p [ngStyle]="applyStylegreen">set style from the Component.</p>  
+<p> <input type="button" (click)="ChangeColor()" value="Change color" /> </p>  
+<p [ngStyle]="applygreenstyle==true?applyStylegreen:appStyleBlue">Change the Style based On Condition..</p>`
+
 })
 export class NgStyleDirevtiveComponent implements OnInit {
-
+ public applygreenstyle:boolean =true;
+ public borderStyle= '1px solid black';
+  applyStylegreen = {
+    'color': 'green',
+    'font-weight': 'bold',
+    'font-size': '45px',
+    'borderBottom': this.borderStyle,
+    'padding': '1rem'
+  }
+  appStyleBlue = {
+    'color': 'blue',
+    'font-weight': 'bold',
+    'font-size': '45px',
+    'borderBottom': this.borderStyle,
+    'padding': '1rem'
+  }; 
   constructor() { }
-
   ngOnInit() {
   }
+  ChangeColor(): void {
+    this.applygreenstyle = !this.applygreenstyle;
+  }  
 
 }
